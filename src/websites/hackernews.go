bfb0af5881687bc51db653edc1241d2ac08b3f0f
crafter-deo <adesolaadeoluwa@gmail.com>
2022-10-01 12:05:20 +0100

    feat(Scraper): Add hacker news scraper

---
+++ src/websites/hackernews.go
@@ -0,0 +1,56 @@
+package websites
+
+import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ScrapeHackernews() []news {
+	doc, err := getHackernewsHtml()
+	if err != nil {
+		return nil
+	}
+
+	var listOfNews []news
+
+	doc.Find("span.titleline").Each(func(i int, s *goquery.Selection) {
+		topicCard := news{}
+
+		topicCard.Headline = s.Find("a").Text()
+		topicCard.Url, _ = s.Find("a").Attr("href")
+		topicCard.Site = "Hacker News"
+
+		listOfNews = append(listOfNews, topicCard)
+	})
+
+	return listOfNews[:5]
+}
+
+func getHackernewsHtml() (*goquery.Document, error) {
+	url := "https://news.ycombinator.com/"
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
+
+}
