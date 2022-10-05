50aebda27f9e490d8f818a5745e8da38030b68f4
crafter-deo <adesolaadeoluwa@gmail.com>
2022-10-06 00:58:32 +0100

    feature: Add route for all news
    This is very inefficient, as the requests take about 40 seconds to complete and they're not always successful. Will work on this

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
-	"log"
+	// "log"
+	"net/http"
 
 	"github.com/Crafter-deo/tech-trends-api/src/websites"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	log.Println(websites.ScrapeCnet())
+	// log.Println(websites.ScrapeCnet())
+	router := gin.Default()
+
+	router.GET("/all", func(ctx *gin.Context) {
+		all_blogs := [][]websites.Blogs{}
+		for _, site := range websites.Sites {
+			switch site {
+			case "cnet":
+				blogs := websites.ScrapeCnet()
+				all_blogs = append(all_blogs, blogs)
+
+			case "codingdojo":
+				blogs := websites.ScrapeCodingdojo()
+				all_blogs = append(all_blogs, blogs)
+
+			case "digitaltrends":
+				blogs := websites.ScrapeDigitaltrends()
+				all_blogs = append(all_blogs, blogs)
+
+			case "hackernews":
+				blogs := websites.ScrapeHackernews()
+				all_blogs = append(all_blogs, blogs)
+
+			case "mashable":
+				blogs := websites.ScrapeMashable()
+				all_blogs = append(all_blogs, blogs)
+
+			case "medium":
+				blogs := websites.ScrapeMedium()
+				all_blogs = append(all_blogs, blogs)
+			}
+		}
+		ctx.JSON(http.StatusOK, all_blogs)
+	})
+	router.Run()
 }
